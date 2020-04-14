66bd96f15e4991c1eaecec3684e47343c19391f7
hackerrank <17colemanm@gmail.com>
2020-04-13 23:10:58 -0400

    a changed the listening port to listen on the currnet environmnet

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req: any, res: any) {
   res.sendFile(__dirname + '/index.html');
@@ -20,6 +21,6 @@ io.on('connection', function(socket: any) {
   });
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
   console.log('listening on *:3000');
 });
--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -17,6 +18,6 @@ io.on('connection', function (socket) {
         io.emit('chat message', msg);
     });
 });
-http.listen(3000, function () {
+http.listen(port, function () {
     console.log('listening on *:3000');
 });
