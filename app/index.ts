7ea3c28a7dc1bd4a3da14957457010bbf6549f2a
Montao Coleman <17colemanm@gmail.com>
2020-04-18 17:10:29 -0400

    -added ES6 to tsconfig for typescript ES6 support -added support to send usernames to the server and save them -user info is deleted from the server on disconnect -added notifications when users join and leave chat -added private messages using the /p command followed by socket id -added ablity for users to click on a name to send a private message -added coloring to specal chat events (join/leave and private messages)

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,23 @@
 import express from "express";//importing express library
+import { Md5 } from 'ts-md5/dist/md5';//temp libaray for hashing
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;//getting the port
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+let port = process.env.PORT || 3000;//getting the port
+
+
+
+
+//part of the test code for the hash map--------------TEST----------------
+// let userMap = new Map();
+
+let users = [{//array of connected users
+  name: '',
+  socket: ''
+}];//initlizing the user array with empty values so that new values of that type
+//can be entered
+
 
 app.use(express.static('public'))//telling the node.js sever where to get static files like css and images
 
@@ -11,21 +25,105 @@ app.get('/', function(req: any, res: any) {
   res.sendFile(__dirname + '/index.html');//assigning the html file where input comes from
 });
 
-//detecting user connections
 io.on('connection', function(socket: any) {
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
+
+  //getting chat messages from users and emitting them to everyone else
+  socket.on('chat message', function(msg: any, ownId: any) {
+    console.log('message: ' + msg + 'from: ' + ownId);
+    io.emit('chat message', msg, ownId);
   });
+
+
+  //detecting user disconnections
+  socket.on('disconnect', function() {
+
+    //seaching for a user in the users array an removing them upon disconnect
+    console.log(users);
+    var count = 0;//count for while loop
+    var found = false;//finding the user in the array
+    while (found == false) {
+
+      //if the socket disconnecting wasnt in the array it was leftover from the last time
+      //the server was ran so we check for undefined here to do nothing when that happens
+      if (users[count] == undefined) {
+        found = true;
+      } else
+        if (users[count].socket == socket.id) {
+          console.log(users[count].name + ' disconnected');//printing to console who left
+          io.emit('join/leave message', users[count].name + ' has left the chat', 'N/A');//sending message to chat who left
+          users.splice(count, 1);//removing the socket entry from the array
+          found = true;//found the entry in the array
+        }
+      count++;
+    }
+  });
+
+  //getting user info once they enter their name and putting it in the array
+  socket.on('joined', function(person: string) {
+
+    var personJSON = {
+      name: person,
+      socket: socket.id
+    }
+
+    //test code for using a hash map in place of an array--------------TEST-----------------
+    //creating the hashcode and logging the userdata into a map
+    // var hashcode = Md5.hashStr(person);
+    // var newCode = '';
+    // for (var i = 0; i < hashcode.length; i++) {
+    //   newCode += hashcode[i];
+    // }
+    // userMap.set(newCode, personJSON);//setting the user info into the map
+
+    users.push(personJSON);//pushing the json data to the array
+    //arrow funciton to sort the json user array by name
+    users.sort((n1, n2) => {
+      if (n1.name > n2.name) {
+        return 1;
+      }
+
+      if (n1.name < n2.name) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    console.log(users);
+    io.emit('join/leave message', person + ' has joined the chat!', socket.id)
+  });
+
+
+  //sends a specific socketID the list of current users connected
+  socket.on('user_list_request', function() {
+    io.to(socket.id).emit('user_list', users);
+    console.log("sending user info to " + socket.id);
+  });
+
+  socket.on('private message', function(msg: string, sendId: string, ownId: string, ownName: string) {
+    //TODO: find the name of who you are sending to in the array from the socket.id
+    //and put it in the message to the sender as in "PM to 'insert name here': 'message'"
+
+    var recName = 'unknown';
+    for (var i = 0; i < users.length; i++) {
+      if (sendId == users[i].socket) {
+        recName = users[i].name;
+      }
+    }
+
+    io.to(sendId).emit('private message', 'PM From ' + ownName + msg);
+    io.to(ownId).emit('private message', 'PM to ' + recName + msg);
+  });
+
+  //prototype code to send the map as a json array string to be---------TEST----------
+  //converted to a map later on using JSON.parse()
+  // socket.on('user_list_request', function() {
+  //   let transitStr = JSON.stringify(Array.from(userMap));
+  //   io.to(socket.id).emit('user_list', transitStr);
+  // });
+
 });
 
-//getting chat messages from users and emitting them to everyone else
-io.on('connection', function(socket: any) {
-  socket.on('chat message', function(msg: any) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
 
 http.listen(port, function() {
   console.log('listening on *:3000');
--- public/html_ref/js/message.js
+++ public/html_ref/js/message.js
@@ -1,29 +1,112 @@
 //promting the user to enter a username before entering chat
 window.onload = load;
-var person;//variable to save the users username
-function load() {
 
-  //making sure the users username is between 3 and 15 characters and not null
-  person = prompt("enter a username between 3 & 15 characters", "anon");
-  while (person == null || person.length < 3 || person.length > 15) {
-    person = prompt("enter a username between 3 & 15 characters", "anon");
+
+var person;//variable to save the users username
+var filter = /^[A-Za-z0-9]+$/;//filter for usernames
+var userList = [];
+var socket = io.connect();//the socket object
+
+function load() {
+  //this line will request the users array in index.ts and send back a user_list function in the socket
+  socket.emit('user_list_request');
+
+  //this filter seciton isnt done yet, it still allows for duplicate names
+  var nameCheck = true;//variable to see if the name is valid
+  while (nameCheck == true) {
+
+    person = prompt("enter a username between 3 & 15 characters (letters & numbers)", "anon");
+
+
+    // cheching null     checking length is between 3 and 15         checking for no special characters
+    if (person == null || person.length < 3 || person.length > 15 || filter.test(person) == false) {
+      alert("invalid username");
+      // person = prompt("enter a username between 3 & 15 characters (letters & numbers)", "anon");
+    } else {
+      nameCheck = false;
+      //sending username to the server
+      socket.emit('joined', person);
+    }
   }
+
+
+
 }
+/*
+  this function prepares a private message in the chat form
+*/
+function displayId(ownId) {
+  $('#m').val('/p ' + ownId + ' ');
+}
+
+
 
 //sending messages typed locally to the socket
 $(function() {
-  var socket = io();
-  $('form').submit(function(e) {//getting the message info from the html form
-    e.preventDefault(); // prevents page reloading after every message
-    socket.emit('chat message', person + ": " + $('#m').val());
-    $('#m').val('');//clearing the users text form after they submit a message
+
+  var userList = [];//the list of current users on the channel
+
+
+  //gets the current user list and saves it in the userList global variable
+  socket.on('user_list', function(users) {
+    for (var i = 0; i < users.length; i++) {
+      userList[i] = users[i];
+    }
+  });
+
+
+  //getting the message from the user and sending it to the server
+  $('form').submit(function(e) { //getting the message info from the html form
+    e.preventDefault(); // prevents page reloading after entring the form
+
+    var ownId = socket.id;//your own id to be sent
+
+    //scanning for if the user sent a private message with /p
+    if ($('#m').val().substring(0, 3) == '/p ') {
+
+      var pMessage = ': ' + $('#m').val().substring(23);//the message to be sent
+      var pId = $('#m').val().substring(3, 23);//the id to send the private message
+      var ownName = person;//senders name
+      socket.emit('private message', pMessage, pId, ownId, ownName);
+
+    } else if ($('#m').val() != '') {//not sending input if the field is empty
+      socket.emit('chat message', person + ': ' + $('#m').val(), ownId);//sending the message to the server
+    }
+    if ($('#m').val().substring(0, 3) != '/p ') {
+      $('#m').val(''); //clearing the users text form after they submit a regular message
+    } else {
+      $('#m').val($('#m').val().substring(0, 23) + ' ');//keeping the PM command after sending a PM
+    }
     return false;
   });
+
+
+  //calling the socket for join/leave events and posting them to the window
+  socket.on('join/leave message', function(msg, ownId) {
+
+    //adding the message to the feed and setting the function to start a pm
+    $('#messages').append($("<li style=\"color:blue;\" onclick='displayId(\"" + ownId + "\");'>").text(msg));
+    window.scrollTo(0, document.body.scrollHeight); // auto scrolling to the bottom of the screen after each chat
+  });
+
+
+
   //calling the socket for chat messages and posting them
-  socket.on('chat message', function(msg) {
+  socket.on('chat message', function(msg, ownId) {
     //checking to make sure the message isnt empty
     if (msg != person + ": ") {
-      $('#messages').append($('<li>').text(msg)); //printing the message & username
+
+      //adding the message to the feed and setting the function to start a pm
+      $('#messages').append($("<li onclick='displayId(\"" + ownId + "\");'>").text(msg));
+      window.scrollTo(0, document.body.scrollHeight); // auto scrolling to the bottom of the screen after each chat
+    }
+  });
+
+  //calling the socket for chat messages and posting them
+  socket.on('private message', function(msg) {
+    //checking to make sure the message isnt empty
+    if (msg != person + ": ") {
+      $('#messages').append($('<li style=\"color:red;\">').text(msg)); //printing the message & username
       window.scrollTo(0, document.body.scrollHeight); // auto scrolling to the bottom of the screen after each chat
     }
   });
